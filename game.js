c3908969ead538840f5989e150dc4794f335dabe
Vikram <49572404+Vikram710@users.noreply.github.com>
2019-06-03 23:24:46 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -5,15 +5,25 @@ canvas.height = canvas.scrollHeight;
 let button1 = document.getElementById("start").addEventListener("click",start);
 let button2 = document.getElementById("pause").addEventListener("click",paused);
 let button3 = document.getElementById("restart").addEventListener("click",start);
+let button4 = document.getElementById("mp").addEventListener("click",mp);
 let img=document.getElementById("img");
 let img1=document.getElementById("img1");
 let img2=document.getElementById("img2");
+let img3=document.getElementById("img3");
+let img4=document.getElementById("img4");
+
 var pause=false;
 var crash=false;
 var k=0;
 var calls=0;
 var array=[];
-
+var mp=false;
+var pcrash=false;
+var bs=1000;
+var gay=0;
+fcrash=false;
+var vs=1000;
+var kay=0;
 
 function paused(){
   document.getElementById("start").innerHTML="RESUME";
@@ -22,12 +32,6 @@ function paused(){
 }
 function open(){
   ctx.drawImage(img1,0,0,480,480);
-}
-function wait(ms){
-  var d1=new Date();
-  var d2=null;
-  do{d2= new Date();}
-  while(d2-d1<ms);
 }
 
 class Obstacle{
@@ -128,7 +132,16 @@ class Duet{
       this.rotate = +this.maxRotate;
     }
     update_d(){
-      this.angle+=this.rotate;     
+      this.angle+=this.rotate;
+      if(pcrash){
+        this.radius=2*50;
+        this.y=350;
+        gay+=1;
+      }  
+      if(fcrash){
+        this.maxRotate=0.24;
+        kay+=1;
+      }
     }
     clear(){
       this.rotate=0;
@@ -144,10 +157,35 @@ class Duet{
       }
       return crash;
     }
+    powerup(other){
+      if(this.xr-this.ballr<other.posX+other.width && this.xr+this.ballr>other.posX &&
+        this.yr-this.ballr<other.posY+other.height && this.yr+this.ballr>other.posY){
+       pcrash=true;
+     }
+    }
+    stop(){
+      this.y=400;
+      this.radius=50;
+    }
+    stopr(){
+      this.maxRotate=0.12;
+      this.rotate=0.12;
+    }
+    flight(other){
+      if(this.xr-this.ballr<other.posX+other.width && this.xr+this.ballr>other.posX &&
+        this.yr-this.ballr<other.posY+other.height && this.yr+this.ballr>other.posY){
+       fcrash=true;
+     }
+     if(this.xb-this.ballr<other.posX+other.width && this.xb+this.ballr>other.posX &&
+        this.yb-this.ballr<other.posY+other.height && this.yb+this.ballr>other.posY){
+       fcrash=true;
+     }
+     return fcrash;
+   }
 
-
+    }
     
-}
+
 
 class Score{
   constructor(x, y){
@@ -180,16 +218,48 @@ class Sound {
   }  
 }
 
+class Power{
+  constructor(x){
+    this.posX=x;
+    this.posY=0;
+    this.speed=1;
+    this.width=50;
+    this.height=50;
+  }
+  draw(ctx,img){
+
+    ctx.drawImage(img,this.posX,this.posY,this.width,this.height);
+  }
+  update_p(){
+    if (temp_score<20){
+      this.speed=1;
+    }
+    else if(temp_score<40){
+      this.speed=2;
+    }
+    else if(temp_score<70){
+      this.speed=3;
+    }
+    else{
+      this.speed=4;
+    }
+    this.posY+=this.speed;
+  }
+}
+
 var sound = new Sound("song.mp3");
 var obj = [];
 var duet = new Duet(50,240,400,0.12,0); 
 new Input();
 var score = new Score(350, 30);
-var final=new Score(200,400);     
+var final=new Score(200,400);  
+holi=[];
+flight=[];
 var count = 0;
 var y=0;
 var temp_score=0;
 var final_score=0;
+
 
 function updategame(){
   for ( var i = 0; i<obj.length; i++){
@@ -210,9 +280,16 @@ function updategame(){
       count=0;
       pause=false;
       obj=[];
+      holi=[];
+      pcrash=false;
       sound.restart();
       crash=false;
       duet.angle=0;
+      gay=0;
+      bs=1000;
+      fcrash=false;
+      vs=1000;
+      kay=0;
       clearTimeout(x);
       return;
       
@@ -240,8 +317,60 @@ function updategame(){
   score.text="SCORE: " + temp_score;
   score.draw(ctx);
 
+  //dlafhjsnkajf
+
+  if(pushpower()>1600 &&pushpower()!=0){
+    z=0;
+    holi.push(new Power(Math.random()*110+150));
+
+  }
+  for(var w=0;w<holi.length;w++){
+  holi[w].draw(ctx , img3);
+  duet.powerup(holi[w]);
+  holi[w].update_p();
+  if(pcrash && gay==1)
+  bs=temp_score;
+
+}
+
+
+if(pushpower1()>1200 &&pushpower1()!=0){
+  zz=0;
+  flight.push(new Power(Math.random()*110+150));
+
+}
+for(var w=0;w<flight.length;w++){
+flight[w].draw(ctx , img4);
+duet.flight(flight[w]);
+flight[w].update_p();
+if(fcrash && kay==1)
+vs=temp_score;
+
+}
+
+
   duet.update_d(ctx);
   duet.draw(ctx);
+
+  
+  if (temp_score-bs>4){
+    holi=[];
+    pcrash=false;
+    duet.stop();
+    bs=1000;
+    gay=0;
+  }
+
+
+  if (temp_score-vs>4){
+    flight=[];
+    fcrash=false;
+    duet.stopr();
+    vs=1000;
+    kay=0;
+  }
+
+  //jaskjfhjh
   duet.clear();
 
   var min = 120;
@@ -286,15 +415,62 @@ function start(){
  
 }
 var q=0;
+var l=1;
+
+
 function store(score){
-  
+  if(q%2==0 && mp){
+    var ndiv = document.createElement('div');
+    document.getElementsByTagName('div')[0].appendChild(ndiv);
+    ndiv.innerHTML="Try: "+(l);
+  }
+  if (mp){
+    array[calls-1]=score;
+    if((calls)%2==0){ 
+      var ndiv = document.createElement('div');
+    document.getElementsByTagName('div')[0].appendChild(ndiv);
+    ndiv.innerHTML="Player 2 : " +array[q];
+    q+=1;
+    l+=0.5;
+
+    }
+    else{
+      var ndiv = document.createElement('div');
+    document.getElementsByTagName('div')[0].appendChild(ndiv);
+    ndiv.innerHTML="Player 1 : " +array[q];
+    q+=1;
+    l+=0.5;
+    ov=false;
+
+    }
+  }
+  else{
   array[calls-1]=score;
   var ndiv = document.createElement('div');
   document.getElementsByTagName('div')[0].appendChild(ndiv);
   ndiv.innerHTML="Try "+ (q+1)+": " +array[q];
-  q+=1;
-  console.log(array);  
+  q+=1;}
+ 
 }
+
+function mp(){  
+  mp=true;
+  alert("You're in 2 players mode.Press restart after a player dies to continue");
+}
+var z=0;
+var zz=0;
+function pushpower(){
+  z=z+Math.random()*3;
+  
+  return Math.floor(z);
+}
+
+function pushpower1(){
+  zz=zz+Math.random()*3;
+  
+  return Math.floor(zz);
+}
+
 
 
 
